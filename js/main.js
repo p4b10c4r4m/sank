9b5c59f9b90d879c67cc6939c03df708dc42446a
Pablo Caram <pablocaram@gmail.com>
2022-05-12 21:09:44 -0300

    desafio eventos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let nombre = prompt("Ingrese nombre de usuario");
+/*let nombre = prompt("Ingrese nombre de usuario");
 console.log(nombre);
 let saludo = `Muchas gracias ${nombre}`;
 alert(saludo)
@@ -26,14 +26,38 @@ alert(resultado)
 const paciente1 = {
     nombre: nombre,
     edad: edad, 
-}
+}*/
 
 //array creado por el profesional medico
 
-const arrayPaciente1 = [paciente1]
+/*const arrayPaciente1 = [paciente1]
 arrayPaciente1.push("Dosha: " + prompt(`Ingrese dosha del paciente ${nombre}`))
 arrayPaciente1.push("Ficha Medica: " + prompt(`Ingrese ficha medica del paciente ${nombre}`)),
 arrayPaciente1.push("Tratamiento: " + prompt(`Ingrese tratamiento medico del paciente ${nombre}`)),
 console.log(arrayPaciente1)
 
-alert(arrayPaciente1.join("\n"))
+alert(arrayPaciente1.join("\n"))*/
+
+
+class ficha {
+    constructor(dosha, fichaMedica, tratamiento) {
+        this.dosha = dosha;
+        this.fichaMedica = fichaMedica;
+        this.tratamiento = tratamiento;
+    }
+}
+
+let arrayPaciente1 = []
+let formPaciente = document.querySelector('#formPaciente')
+
+formPaciente.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let dosha = document.querySelector("#idDosha").value 
+    let fichaMedica = document.querySelector("#idFicha").value 
+    let tratamiento = document.querySelector("#idTratamiento").value 
+    
+   const arrayficha = new ficha(dosha, fichaMedica, tratamiento)
+   arrayPaciente1.push(arrayficha)
+   console.log(arrayPaciente1)
+   formPaciente.reset()
+})
